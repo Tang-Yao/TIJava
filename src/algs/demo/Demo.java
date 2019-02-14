24f306b1b79905d9b17adb06eec30376a5a56010
tangyao <1209985002@qq.com>
2019-02-14 13:59:56 +0800

    Update Demo.java
    this is a demo to test githubdesktop.

--- src/algs/demo/Demo.java
+++ src/algs/demo/Demo.java
@@ -6,11 +6,15 @@ package algs.demo;
 public class Demo {
     public static void main(String[] args) {
         int i = 1;
+        int sum = 0;
         for (int j = 0; j < 1000; j++) {
             i = i++;
             //System.out.print(i + " ");
         }
-        //System.out.println();
+        for (int begin = 0; begin < 100; begin++) {
+            sum = sum + begin;
+        }
+        System.out.println(sum);
         System.out.println(i);
     }
 }
